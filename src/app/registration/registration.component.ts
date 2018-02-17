312fa36bb9a0e20faf0eac251d270ad482af3488
Uliana Pavelko <uliana@getwhichit.com>
2018-02-17 15:50:12 +0000

    added email validation

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -10,14 +10,14 @@ import { Observable } from 'rxjs/Observable';
 export class RegistrationComponent {
   public registrationForm: FormGroup;
 
-  constructor( private fb: FormBuilder ) {
+  constructor(private fb: FormBuilder) {
     this.createForm();
   }
 
   private createForm() {
     this.registrationForm = this.fb.group({
-      email: ['', Validators.required ],
-      password: ['', [ Validators.required, Validators.minLength(5)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(5)]],
       passwordCopy: ['', [Validators.required, Validators.minLength(5), this.checkPassword]]
     });
   }
@@ -27,20 +27,14 @@ export class RegistrationComponent {
     console.log(this.registrationForm.value);
   }
 
-  public checkPassword = (control): Observable<{[key: string]: string }> => {
-if (!control || !control.value || !control.value.length) {
-  return null;
- }
+  public checkPassword = (control): Observable<{ [key: string]: string }> => {
+    if (!control || !control.value || !control.value.length) {
+      return null;
+    }
 
-if (control.value !== this.registrationForm.value.password) {
-  return Observable.create({ PasswordMatch: 'Fields do not match' });
-}
-  }
-
-  public passwordMatch(formValues: any): boolean {
-    console.log('formValues')
-    console.log(formValues)
-    return false;
+    if (control.value !== this.registrationForm.value.password) {
+      return Observable.create({ PasswordMatch: 'Fields do not match' });
+    }
   }
 
 }
