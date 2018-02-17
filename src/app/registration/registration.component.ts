889c9ab562d02fd80e24166305b9ca2b38280fa8
Uliana Pavelko <uliana@getwhichit.com>
2018-02-17 15:39:11 +0000

    added form validation

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-registration',
@@ -16,13 +17,30 @@ export class RegistrationComponent {
   private createForm() {
     this.registrationForm = this.fb.group({
       email: ['', Validators.required ],
-      passWord: ['', Validators.required],
-      passWordCopy: ['', Validators.required]
+      password: ['', [ Validators.required, Validators.minLength(5)]],
+      passwordCopy: ['', [Validators.required, Validators.minLength(5), this.checkPassword]]
     });
   }
 
   public register() {
     console.log(this.registrationForm.status);
-    console.log(this.registrationForm);
+    console.log(this.registrationForm.value);
   }
+
+  public checkPassword = (control): Observable<{[key: string]: string }> => {
+if (!control || !control.value || !control.value.length) {
+  return null;
+ }
+
+if (control.value !== this.registrationForm.value.password) {
+  return Observable.create({ PasswordMatch: 'Fields do not match' });
+}
+  }
+
+  public passwordMatch(formValues: any): boolean {
+    console.log('formValues')
+    console.log(formValues)
+    return false;
+  }
+
 }
