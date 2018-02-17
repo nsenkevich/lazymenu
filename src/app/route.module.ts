ae8768a47a682206a199fca1bb104236c4d58dcd
Nikolai <nikolai.senkevich@feelunique.com>
2018-02-17 16:03:14 +0000

    added menu

--- src/app/route.module.ts
+++ src/app/route.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MenuComponent } from './menu/menu.component';
-
+import { AppComponent } from './app/app.component';
 
 const routes: Routes = [
     {
       path: '',
-      component: MenuComponent
+      component: AppComponent
     },
     {
       path: 'menu',
