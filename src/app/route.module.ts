e740d6762f52f9fef6ac98a4d409f7d4785476b6
Nikolai <nikolai.senkevich@feelunique.com>
2018-02-19 20:32:01 +0000

    added recipe component

--- src/app/route.module.ts
+++ src/app/route.module.ts
@@ -4,25 +4,14 @@ import { MenuComponent } from './menu/menu.component';
 import { AppComponent } from './app/app.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { PageNotFoundComponent } from './app/page-not-found.component';
+import { RecipeComponent } from './recipe/recipe.component';
 
 const routes: Routes = [
-    {
-      path: '',
-      component: AppComponent
-    },
-    {
-      path: 'menu',
-      component: MenuComponent
-    },
-    {
-      path: 'registration',
-      component: RegistrationComponent
-    },
-    {
-      path: '**',
-      pathMatch: 'full',
-      component: PageNotFoundComponent
-    }
+    {path: '', component: AppComponent},
+    {path: 'menu', component: MenuComponent},
+    {path: 'registration', component: RegistrationComponent},
+    {path: '**', pathMatch: 'full', component: PageNotFoundComponent},
+    {path: 'recipe/:id', component: RecipeComponent}
   ];
 
 @NgModule({
