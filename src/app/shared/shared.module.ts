030602b1d99826015679565e47fb13fbdf86898d
Uliana Pavelko <upavelko@gmail.com>
2018-03-19 13:45:44 +0000

    added mixings folder for scss reuse

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,4 +1,3 @@
-import { AngularFirestoreModule } from 'angularfire2/firestore';
 // Why? SharedModule exists to make commonly used components, directives and pipes available for use in the templates
 // of components in many other modules
 
@@ -6,6 +5,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
+import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { MaterialModule } from '../material.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
