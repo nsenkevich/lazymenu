df6708f073018269278dcaecdb0d23e2b5a6042a
Uliana Pavelko <upavelko@gmail.com>
2018-05-31 13:40:53 +0100

    download PDF poc

--- src/app/recipes/recipe-details/recipe-details.component.ts
+++ src/app/recipes/recipe-details/recipe-details.component.ts
@@ -1,7 +1,8 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { RecipeService } from '../recipe.service';
 import { Recipe } from '../recipe.model';
+import * as jsPDF from 'jspdf';
 
 @Component({
   selector: 'app-recipe-details',
@@ -16,15 +17,34 @@ export class RecipeDetailsComponent implements OnInit {
   private route: ActivatedRoute;
   private recipeService: RecipeService;
 
-  public constructor(route: ActivatedRoute, recipeService: RecipeService) {
+  @ViewChild('page') page: ElementRef;
+
+  constructor(route: ActivatedRoute, recipeService: RecipeService) {
     this.recipeService = recipeService;
     this.route = route;
     this.showVideo = true;
   }
 
-  public ngOnInit() {
+  ngOnInit() {
     this.route.params.subscribe(params => this.recipeId = params.id);
     this.recipe = this.recipeService.getRecipe(this.recipeId).valueChanges();
+  }
+
+  public processPdfData(recipeData: any): void {
+    console.log(recipeData)
+
+    const doc = new jsPDF();
+    const specialElementHandlers = {
+      '#editor': function (element, renderer) {
+        return true;
+      }
+    };
+    const content = this.page.nativeElement;
+    doc.fromHTML(content, 15, 15, {
+      'width': 190,
+      'elementHandlers': specialElementHandlers
+    });
+    doc.save(recipeData.name + '.pdf');
   }
 
 }
