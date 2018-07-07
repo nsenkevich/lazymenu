58d82a95ff6b41796cb3ac34c6d3bdbadfece70d
Nikolai SEnkevich <nsenkevich@gmail.com>
2018-07-08 00:48:02 +0100

    moved all fairbase config to travis env

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: true,
+    firebase: {
+        apiKey: process.env.apiKey,
+        authDomain: process.env.authDomain,
+        databaseURL: process.env.databaseURL,
+        projectId: process.env.projectId,
+        storageBucket: process.env.storageBucket,
+        messagingSenderId: process.env.messagingSenderId
+      }
+};
